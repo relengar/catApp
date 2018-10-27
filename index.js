83c33ea248a1358e4e519df461741cc83fce1830
relengar <valquendil@gmail.com>
2018-10-27 09:15:43 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use(express.static('client/build'));
+
+const path = require('path');
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve('client', 'build', 'index.html'));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Listening on port`, PORT);
+});
