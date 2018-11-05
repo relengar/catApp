3ccdc477be1d5509ec9d6a7b91fa6d8604d347bb
relengar <valquendil@gmail.com>
2018-11-05 18:42:23 +0100

    config to run the app

---
+++ config/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI: 'mongodb://dude:zaq12wsx@ds145562.mlab.com:45562/catapp_test',
+    cookieKey: 'secret',
+    catApiKey: '2ab6fa40-e0c5-441a-84cf-2413c9d05a63',
+}
+
